9eb52b2fc6aba1cbbe9a0cdd775b9d1ca2af97cf
khushi-66 <sahujii8277@gmail.com>
2026-02-09 22:14:58 +0530

    React Learning Day-1

---
+++ myviteapp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import Chai from './chai.jsx';
+
+function App() {
+     return (
+   <Chai />
+  );
+}
+
+export default App;
